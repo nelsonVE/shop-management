91d0d4ffb304e72d572e0b8516a43108e0d07c1b
Nelson Gonçalves <nelsonmggomes@gmail.com>
2021-04-02 22:57:05 +0100

    feature: First commit and login base.

---
+++ source/backend/authentication/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+
+from rest_framework_simplejwt import views as jwt_views
+
+from . import views
+
+app_name = 'authentication'
+
+urlpatterns = [
+
+    # Token paths
+    path('token/obtain/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),  # override sjwt stock token
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+]
